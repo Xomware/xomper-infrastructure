90d2a9e974784ae94b9fd2b17718eff744055096
domgiordano <44783934+domgiordano@users.noreply.github.com>
2026-04-08 10:35:01 -0400

    fix: remove unsupported tags from SNS platform application (#97)

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -10,5 +10,4 @@ resource "aws_sns_platform_application" "apns" {
   apple_platform_team_id   = var.apns_team_id
   apple_platform_bundle_id = var.apns_bundle_id
 
-  tags = merge(local.standard_tags, { "name" = "${var.app_name}-apns" })
 }
